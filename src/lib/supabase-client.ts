848edb5aabf8970041349a501d8d02297b3ec2fa
Em Snook <me@michaelsnook.com>
2024-08-18 22:55:17 +0530

    Add supabase client, cli, types, env, aliases

---
+++ src/lib/supabase-client.ts
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+import { Database } from '../types/supabase'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const publicKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY
+
+const supabase = createClient<Database>(supabaseUrl, publicKey)
+
+export default supabase
