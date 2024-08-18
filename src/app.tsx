bc499d5fe6033aeeb9226b3520415c2ed3f0423d
Em Snook <me@michaelsnook.com>
2024-08-19 01:10:40 +0530

    Add daisy, css, theme, layout

--- src/app.tsx
+++ src/app.tsx
@@ -5,15 +5,19 @@ import './app.css'
 
 export default function App() {
 	return (
-		<Router
-			root={props => (
-				<>
-					{/*<Sidebar />*/}
-					<Suspense>{props.children}</Suspense>
-				</>
-			)}
-		>
-			<FileRoutes />
-		</Router>
+		<div class="flex min-h-screen flex-row bg-[hsl(210,100,15)] text-white">
+			<Router
+				root={props => (
+					<>
+						{/*<Sidebar />*/}
+						<div class="@container mx-auto w-full max-w-[1100px] px-[1%] py-20">
+							<Suspense>{props.children}</Suspense>
+						</div>
+					</>
+				)}
+			>
+				<FileRoutes />
+			</Router>
+		</div>
 	)
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-};
+	mode: 'jit',
+	content: ['./src/**/*.{js,ts,jsx,tsx,html}'],
+	theme: {
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+		},
+		extend: {
+			fontFamily: {
+				sans: [
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'"Segoe UI"',
+					'"Helvetica Neue"',
+					'Roboto',
+					'Oxygen-Sans',
+					'Ubuntu',
+					'Cantarell',
+					'sans-serif',
+				],
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/container-queries'),
+		require('@tailwindcss/forms'),
+		require('daisyui'),
+	],
+	daisyui: {
+		styled: true,
+		themes: [
+			{
+				light: {
+					...require('daisyui/src/theming/themes')['light'],
+					primary: '#663399',
+				},
+			},
+			{
+				dark: {
+					...require('daisyui/src/theming/themes')['dark'],
+					primary: '#663399',
+				},
+			},
+		],
+		base: true,
+		utils: true,
+		logs: true,
+	},
+}
